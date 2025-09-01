e738e57312b52aa0eb461e3b6c87d574b2ca8bd1
mariaherliana1 <mariawork174@gmail.com>
2025-09-01 15:05:19 +0700

    Update auto-anna.py

--- auto-anna.py
+++ auto-anna.py
@@ -1,19 +1,19 @@
 from config import CONFIG
 from src.csv_processing import (
-    process_console_csv,
     process_dashboard_csv,
     save_merged_csv,
 )
 
 
 def __main__():
-    print(f"Starting Auto-Anna CSV merger")
+    print("Starting Auto-Anna (Dashboard Only)")
     for files in CONFIG:
-        print(f"> Merging files for client {files.client}")
-        call_details = process_dashboard_csv(files.dashboard, files.carrier, client=files.client)
-        call_details = process_console_csv(files.console, files.carrier, call_details, client=files.client)
+        print(f"> Processing dashboard file for client {files.client}")
+        call_details = process_dashboard_csv(
+            files.dashboard, files.carrier, client=files.client
+        )
         save_merged_csv(call_details, files.output)
-    print("All files merged successfully")
+    print("All files processed successfully")
 
 
 __main__()
